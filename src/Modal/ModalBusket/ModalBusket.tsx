c9665f65ff9fd3f4dc51cc4ed5188e3440d2ce4c
NooBi4ek <gajdukmaksim710@gmail.com>
2023-01-09 20:54:22 +0300

    Fix bug deleted item on order

--- src/Modal/ModalBusket/ModalBusket.tsx
+++ src/Modal/ModalBusket/ModalBusket.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addcount, deleteOrder } from '../../store/internet_shop_slice.ts';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addcount,
+  deleteOrder,
+  afterdeleteOrder,
+} from "../../store/internet_shop_slice.ts";
 import {
   Basket_button,
   Basket_div,
   Basket_img,
   Basket_wrapper,
-} from '../../styled/styled-busket/SBusket';
+} from "../../styled/styled-busket/SBusket";
 
 const ModalBusket = ({ orders, id }) => {
   const dispatch = useDispatch();
   const phones = useSelector((state) => state.shop.phones);
   return (
     <Basket_wrapper>
-      <Basket_img src={'./img/' + orders.img} />
+      <Basket_img src={"./img/" + orders.img} />
       <Basket_div>
         <div>{orders.name}</div>
         <div>{orders.price * orders.count} UAH</div>
@@ -27,7 +31,8 @@ const ModalBusket = ({ orders, id }) => {
         </Basket_button>
         <Basket_button
           onClick={() => {
-            dispatch(deleteOrder({ orders, phones }));
+            dispatch(deleteOrder({ orders }));
+            dispatch(afterdeleteOrder());
           }}
         >
           Delete count
--- src/store/internet_shop_slice.ts
+++ src/store/internet_shop_slice.ts
@@ -1,19 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { IPhones } from '../models/models';
+import { createSlice } from "@reduxjs/toolkit";
+import { IPhones } from "../models/models";
 
 const shopSlice: IPhones = createSlice({
-  name: 'shop',
+  name: "shop",
   initialState: {
     phones: [
       {
         id: 1,
-        name: 'Apple iPhone 11 128GB',
-        company: 'Apple',
-        short_desc: 'something',
-        img: 'Apple_Iphone11_128.jpg',
-        threads_name: 'Apple A13 Bionic',
-        front_camera: '12MP',
-        main_camera: '12MP',
+        name: "Apple iPhone 11 128GB",
+        company: "Apple",
+        short_desc: "something",
+        img: "Apple_Iphone11_128.jpg",
+        threads_name: "Apple A13 Bionic",
+        front_camera: "12MP",
+        main_camera: "12MP",
         count: 1,
         quantity_threads: 6,
         memory: 128,
@@ -23,13 +23,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 2,
-        name: 'Apple iPhone 13 128GB',
-        company: 'Apple',
-        short_desc: 'something',
-        img: 'Apple_Iphone13_128.jpg',
-        threads_name: 'Apple A14 Bionic',
-        front_camera: '12MP',
-        main_camera: '12MP',
+        name: "Apple iPhone 13 128GB",
+        company: "Apple",
+        short_desc: "something",
+        img: "Apple_Iphone13_128.jpg",
+        threads_name: "Apple A14 Bionic",
+        front_camera: "12MP",
+        main_camera: "12MP",
         price: 15000,
         count: 1,
         quantity_threads: 6,
@@ -39,13 +39,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 3,
-        name: 'Apple iPhone 13 PRO 128GB',
-        company: 'Apple',
-        short_desc: 'something',
-        img: 'Apple_Iphone13_pro_128.jpg',
-        threads_name: 'Apple A15 Bionic',
-        front_camera: '12MP',
-        main_camera: '12MP',
+        name: "Apple iPhone 13 PRO 128GB",
+        company: "Apple",
+        short_desc: "something",
+        img: "Apple_Iphone13_pro_128.jpg",
+        threads_name: "Apple A15 Bionic",
+        front_camera: "12MP",
+        main_camera: "12MP",
         price: 17000,
         count: 1,
         quantity_threads: 6,
@@ -55,13 +55,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 4,
-        name: 'Apple iPhone 13 PRO MAX 128GB',
-        company: 'Apple',
-        short_desc: 'something',
-        img: 'Apple_Iphone13_pro_max_128.jpg',
-        threads_name: 'Apple A15 Bionic',
-        front_camera: '12MP',
-        main_camera: '12MP',
+        name: "Apple iPhone 13 PRO MAX 128GB",
+        company: "Apple",
+        short_desc: "something",
+        img: "Apple_Iphone13_pro_max_128.jpg",
+        threads_name: "Apple A15 Bionic",
+        front_camera: "12MP",
+        main_camera: "12MP",
         price: 20000,
         count: 1,
         quantity_threads: 6,
@@ -71,13 +71,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 5,
-        name: 'Infinix Note 10 PRO 8/128GB',
-        company: 'Infinix',
-        short_desc: 'something',
-        img: 'Infinix_note10_pro_8_128.jpg',
-        threads_name: 'MediaTek Helio G95',
-        main_camera: '64MP + 8MP + 2MP + 2MP',
-        front_camera: '16MP',
+        name: "Infinix Note 10 PRO 8/128GB",
+        company: "Infinix",
+        short_desc: "something",
+        img: "Infinix_note10_pro_8_128.jpg",
+        threads_name: "MediaTek Helio G95",
+        main_camera: "64MP + 8MP + 2MP + 2MP",
+        front_camera: "16MP",
         price: 7000,
         count: 1,
         quantity_threads: 8,
@@ -87,13 +87,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 6,
-        name: 'Motorola Moto G60 6/128GB',
-        company: 'Motorola',
-        short_desc: 'something',
-        img: 'Motorola_moto_g60_6_128.jpg',
-        threads_name: 'Qualcomm Snapdragon 732G',
-        main_camera: '108MP + 8MP + 2MP',
-        front_camera: '32MP',
+        name: "Motorola Moto G60 6/128GB",
+        company: "Motorola",
+        short_desc: "something",
+        img: "Motorola_moto_g60_6_128.jpg",
+        threads_name: "Qualcomm Snapdragon 732G",
+        main_camera: "108MP + 8MP + 2MP",
+        front_camera: "32MP",
         price: 9000,
         count: 1,
         quantity_threads: 8,
@@ -103,13 +103,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 7,
-        name: 'Samsung Galaxy A32 4/64GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_a32_4_64.jpg',
-        threads_name: 'MediaTek Helio G80',
-        main_camera: '64MP + 8MP + 5MP + 5MP',
-        front_camera: '20MP',
+        name: "Samsung Galaxy A32 4/64GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_a32_4_64.jpg",
+        threads_name: "MediaTek Helio G80",
+        main_camera: "64MP + 8MP + 5MP + 5MP",
+        front_camera: "20MP",
         price: 5000,
         count: 1,
         quantity_threads: 8,
@@ -119,13 +119,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 8,
-        name: 'Samsung Galaxy A32 4/128GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_a32_4_128.jpg',
-        threads_name: 'MediaTek Helio G80',
-        main_camera: '64MP + 8MP + 5MP + 5MP',
-        front_camera: '20MP',
+        name: "Samsung Galaxy A32 4/128GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_a32_4_128.jpg",
+        threads_name: "MediaTek Helio G80",
+        main_camera: "64MP + 8MP + 5MP + 5MP",
+        front_camera: "20MP",
         price: 10000,
         count: 1,
         quantity_threads: 8,
@@ -135,13 +135,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 9,
-        name: 'Samsung Galaxy A53 5G 6/128GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_a53_5g_6_128.jpg',
-        threads_name: 'Samsung Exynos 1280',
-        main_camera: '64MP + 12MP + 5MP + 5MP',
-        front_camera: '32MP',
+        name: "Samsung Galaxy A53 5G 6/128GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_a53_5g_6_128.jpg",
+        threads_name: "Samsung Exynos 1280",
+        main_camera: "64MP + 12MP + 5MP + 5MP",
+        front_camera: "32MP",
         price: 17000,
         count: 1,
         quantity_threads: 8,
@@ -151,13 +151,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 10,
-        name: 'Samsung Galaxy M32 6/128GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_m32_6_128.jpg',
-        threads_name: 'MediaTek Helio G80',
-        main_camera: '64MP + 8MP + 2MP + 2MP',
-        front_camera: '20MP',
+        name: "Samsung Galaxy M32 6/128GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_m32_6_128.jpg",
+        threads_name: "MediaTek Helio G80",
+        main_camera: "64MP + 8MP + 2MP + 2MP",
+        front_camera: "20MP",
         price: 17000,
         count: 1,
         quantity_threads: 8,
@@ -167,13 +167,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 11,
-        name: 'Samsung Galaxy M52 5G 6/128GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_m52_5g_6_128.jpg',
-        threads_name: 'Qualcomm Snapdragon 778G',
-        main_camera: '64MP + 12MP + 5MP',
-        front_camera: '32MP',
+        name: "Samsung Galaxy M52 5G 6/128GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_m52_5g_6_128.jpg",
+        threads_name: "Qualcomm Snapdragon 778G",
+        main_camera: "64MP + 12MP + 5MP",
+        front_camera: "32MP",
         price: 11000,
         count: 1,
         quantity_threads: 8,
@@ -183,13 +183,13 @@ const shopSlice: IPhones = createSlice({
       },
       {
         id: 12,
-        name: 'Samsung Galaxy M53 5G 6/128GB',
-        company: 'Samsung',
-        short_desc: 'something',
-        img: 'Samsung_galaxy_m53_5g_6_128.jpg',
-        threads_name: 'Qualcomm Snapdragon 778G',
-        main_camera: '64MP + 12MP + 5MP',
-        front_camera: '32MP',
+        name: "Samsung Galaxy M53 5G 6/128GB",
+        company: "Samsung",
+        short_desc: "something",
+        img: "Samsung_galaxy_m53_5g_6_128.jpg",
+        threads_name: "Qualcomm Snapdragon 778G",
+        main_camera: "64MP + 12MP + 5MP",
+        front_camera: "32MP",
         price: 17000,
         count: 1,
         quantity_threads: 8,
@@ -199,23 +199,22 @@ const shopSlice: IPhones = createSlice({
       },
     ],
     categories: [
-      { id: 1, namecat: 'All' },
-      { id: 2, namecat: 'Apple' },
-      { id: 3, namecat: 'Samsung' },
-      { id: 4, namecat: 'Infinix' },
+      { id: 1, namecat: "All" },
+      { id: 2, namecat: "Apple" },
+      { id: 3, namecat: "Samsung" },
+      { id: 4, namecat: "Infinix" },
     ],
-    All_category: false,
     orders: [],
     filter_phone: [],
+    category: "",
     versus_Phone: [],
     sum: 0,
   },
   reducers: {
     filterCategories(state, action) {
       state.filter_phone = [];
-      if (action.payload.el.namecat === 'All') {
+      if (action.payload.el.namecat === "All") {
         state.phones.map((el) => state.filter_phone.push(el));
-        state.All_category = true;
       } else {
         state.phones.filter((phones) => {
           return (
@@ -223,16 +222,14 @@ const shopSlice: IPhones = createSlice({
             state.filter_phone.push(phones)
           );
         });
-        state.All_category = false;
       }
+      state.category = action.payload.el.namecat;
+      console.log(state.category);
     },
     startPhone(state) {
       state.filter_phone = [];
       state.phones.map((el) => state.filter_phone.push(el));
-      state.All_category = true;
-    },
-    Countsum(state) {
-      state.orders.forEach((el) => (state.sum += el.count * el.price));
+      state.category = "All";
     },
     addToOrder(state, action) {
       let isArr = false;
@@ -248,17 +245,17 @@ const shopSlice: IPhones = createSlice({
         state.filter_phone = state.phones.map((el) =>
           el.id === action.payload.phone.id
             ? { ...el, click: (el.click = true) }
-            : el,
+            : el
         );
       }
     },
     afterAddOrder(state, action) {
-      if (state.All_category !== true) {
+      if (state.category != "All") {
         state.filter_phone = [];
         state.phones.forEach((el) =>
           el.company === action.payload.phone.company
             ? state.filter_phone.push(el)
-            : null,
+            : null
         );
       } else {
         state.filter_phone = [];
@@ -270,27 +267,27 @@ const shopSlice: IPhones = createSlice({
         state.orders = state.orders.map((el) =>
           el.id === action.payload.orders.id
             ? { ...el, count: el.count - 1 }
-            : el,
+            : el
         );
       } else {
         state.filter_phone = state.phones.map((el) =>
           el.id === action.payload.orders.id
             ? { ...el, click: (el.click = false) }
-            : el,
+            : null
         );
-        if (state.All_category !== true) {
-          state.filter_phone = [];
-          state.phones.filter((el) =>
-            el.company === action.payload.orders.company
-              ? state.filter_phone.push(el)
-              : el,
-          );
-        } else {
-          state.filter_phone = [];
-          state.phones.forEach((el) => state.filter_phone.push(el));
-        }
         state.orders = state.orders.filter(
-          (el) => el.id !== action.payload.orders.id,
+          (el) => el.id !== action.payload.orders.id
+        );
+      }
+    },
+    afterdeleteOrder(state) {
+      if (state.category == "All") {
+        state.filter_phone = [];
+        state.phones.map((el) => state.filter_phone.push(el));
+      } else {
+        state.filter_phone = [];
+        state.phones.map(
+          (el) => el.company === state.category && state.filter_phone.push(el)
         );
       }
     },
@@ -306,23 +303,23 @@ const shopSlice: IPhones = createSlice({
         state.filter_phone = state.phones.map((el) =>
           el.id === action.payload.phone.id
             ? { ...el, click_versus: (el.click_versus = true) }
-            : el,
+            : el
         );
       }
     },
     DeleteInVersus(state, action) {
       state.versus_Phone = state.versus_Phone.filter(
-        (el) => el.id !== action.payload.phone.id,
+        (el) => el.id !== action.payload.phone.id
       );
       state.phones = state.phones.map((el) =>
         el.id === action.payload.phone.id
           ? { ...el, click_versus: (el.click_versus = false) }
-          : el,
+          : el
       );
     },
     addcount(state, action) {
       state.orders = state.orders.map((el) =>
-        el.id === action.payload.id ? { ...el, count: el.count + 1 } : el,
+        el.id === action.payload.id ? { ...el, count: el.count + 1 } : el
       );
     },
   },
@@ -337,5 +334,6 @@ export const {
   filterCategories,
   startPhone,
   afterAddOrder,
+  afterdeleteOrder,
 } = shopSlice.actions;
 export default shopSlice.reducer;
